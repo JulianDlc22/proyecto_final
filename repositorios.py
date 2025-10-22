da912c54f5ddaf6a01868e4eb2e691fec0dff834
JulianDlc <ja.dlcastillo@gmail.com>
2025-10-21 20:44:56 -0600

    retroalimentacion de ingreso(no funciona, aun)

---
+++ repositorios.py
@@ -0,0 +1,49 @@
+import sqlite3
+from Clases_Constructoras import Empleado
+
+DB_NAME = "escuela.db"
+
+class RepositorioEmpleado:
+
+    def guardar(self, empleado: Empleado):
+        pass
+
+    def listar(self):
+        pass
+
+class RepositorioEmpleadoSQLite(RepositorioEmpleado):
+
+    def __init__(self , base_datos = DB_NAME):
+        self.base_datos = base_datos
+        self.crear_tabla()
+
+    def conexion(self):
+        conn = sqlite3.connect(self.base_datos)
+        conn.row_factory = sqlite3.Row
+        return conn
+
+    def crear_tabla(self):
+        with self.conexion() as conn:
+            conn.execute("""CREATE TABLE IF NOT EXISTS empleados(  
+                        id_empleado INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                        DPI INTEGER NOT NULL,
+                        primer_nombre TEXT NOT NULL,
+                        segundo_nombre TEXT NOT NULL,
+                        otro_nombre TEXT NOT NULL,
+                        primer_apellido TEXT NOT NULL,
+                        segundo_apellido TEXT NOT NULL,
+                        otro_apellido TEXT NOT NULL,
+                        usuario TEXT NOT NULL,
+                        contraseña TEXT NOT NULL,
+                        fecha_nacimiento TEXT NOT NULL);
+                        
+                        """)
+
+    def guardar(self, empleado: Empleado):
+        with self.conexion() as conn:
+            conn.execute(""" INSERT INTO empleados (DPI, primer_nombre, segundo_nombre, otro_nombre, primer_apellido, segundo_apellido, otro_apellido, usuario, contraseña, fecha_nacimiento) 
+                             VALUES (?,?,?,?,?,?,?,?,?,?)""",(empleado.DPI, empleado.primer_nombre, empleado.segundo_nombre,
+                                                              empleado.otro_nombre, empleado.primer_apellido, empleado.segundo_apellido,
+                                                              empleado.otro_apellido,empleado.usuario, empleado.contraseña, empleado.fecha_nacimiento ))
+            conn.commit()
+
--- ventanas_ingreso.py
+++ ventanas_ingreso.py
@@ -1,9 +1,15 @@
+from operator import truediv
+
 import flet as ft
-import Clases_Constructoras as Empleado
+from Clases_Constructoras import Empleado
+from repositorios import RepositorioEmpleadoSQLite
 
 class IngresoEmpleado:
 
-    def ingreso_empleado(page: ft.Page):
+    def __init__(self):
+        self.repo = RepositorioEmpleadoSQLite()
+
+    def ingreso_empleado(self, page: ft.Page):
         page.title = "Ingreso de Empleados"
         page.bgcolor = "white"
         page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
@@ -11,30 +17,48 @@ class IngresoEmpleado:
         page.add(titulo)
 
         # --- Campos de texto ---
+        DPI = ft.TextField(label="DPI", width=500, color="black", border_color="#0E4B40", text_size=15,keyboard_type=ft.KeyboardType.TEXT)
         primer_nombre = ft.TextField(label="Primer Nombre", hint_text="", width=500, color="black",border_color="#0E4B40", text_size=15, keyboard_type=ft.KeyboardType.TEXT)
         segundo_nombre = ft.TextField(label="Segundo Nombre", width=500, color="black", border_color="#0E4B40",text_size=15, keyboard_type=ft.KeyboardType.TEXT)
         otro_nombre = ft.TextField(label="Otro Nombre", hint_text="Opcional", width=500, color="black",border_color="#0E4B40", text_size=15, keyboard_type=ft.KeyboardType.TEXT)
         primer_apellido = ft.TextField(label="Primer Apellido", width=500, color="black", border_color="#0E4B40",text_size=15, keyboard_type=ft.KeyboardType.TEXT)
         segundo_apellido = ft.TextField(label="Segundo Apellido", width=500, color="black", border_color="#0E4B40",text_size=15, keyboard_type=ft.KeyboardType.TEXT)
         otro_apellido = ft.TextField(label="Apellido de Casada", hint_text="Opcional", width=500, color="black",border_color="#0E4B40", text_size=15, keyboard_type=ft.KeyboardType.TEXT)
-        user = ft.TextField(label="Usuario", width=500, color="black", border_color="#0E4B40", text_size=15,keyboard_type=ft.KeyboardType.TEXT)
-        password = ft.TextField(label="Contraseña", width=500, color="black", border_color="#0E4B40", text_size=15,keyboard_type=ft.KeyboardType.TEXT)
-        DPI = ft.TextField(label="DPI", width=500, color="black", border_color="#0E4B40", text_size=15, keyboard_type=ft.KeyboardType.TEXT)
+        usuario = ft.TextField(label="Usuario", width=500, color="black", border_color="#0E4B40", text_size=15,keyboard_type=ft.KeyboardType.TEXT)
+        contraseña = ft.TextField(label="Contraseña", width=500, color="black", border_color="#0E4B40", text_size=15,keyboard_type=ft.KeyboardType.TEXT)
 
-        # --- Campo para mostrar resultados ---
-        resultado = ft.Text()
-
-        # --- Función que se ejecuta al presionar el botón ---
-        def guardar_empleado(e):
-            # Aquí podrías crear un objeto Empleado si lo tienes definido
-            # emp = Empleado.Empleado(primer_nombre.value, segundo_nombre.value, ...)
-            resultado.value = f"Empleado {primer_nombre.value} guardado"
+        def mostrar_mensaje(titulo, mensaje, tipo="info"):
+            color = "#0E4B40" if tipo == "exito" else "#B00020"  # verde para éxito, rojo para error
+            snackbar = ft.SnackBar(
+                ft.Text(f"{titulo}: {mensaje}", color="white"),
+                bgcolor=color,
+                open=True,
+                duration=5000  # milisegundos: 3 segundos
+            )
+            page.snack_bar = snackbar
+            page.snack_bar.open = True
             page.update()
 
-        # --- Layout: agregamos los elementos a la página ---
-        page.add(primer_nombre, segundo_nombre, otro_nombre, primer_apellido, segundo_apellido, otro_apellido, user,
-                 password, DPI)
+        # --- Función que se ejecuta al presionar el botón ---
+
+        def guardar_empleado(e):
+            if not (DPI.value or primer_nombre.value or segundo_nombre.value or primer_apellido.value or segundo_apellido.value or usuario.value or contraseña.value ):
+                mostrar_mensaje("Error", "Debe ingresar todos los campos obligatorios")
+                return
+
+            empleado = Empleado(DPI.value, primer_nombre.value, segundo_nombre.value, otro_nombre.value, primer_apellido.value, segundo_apellido.value,otro_apellido.value, usuario.value, contraseña.value, "")
+            self.repo.guardar(empleado)
+            mostrar_mensaje("Exito", "Empleado guardado correctamente", tipo="exito")
+
+        boton_guardar = ft.ElevatedButton(text="Guardar Empleado", on_click=guardar_empleado, color="black", bgcolor="#0E4B40")
+
+
+        # --- agregamos los elementos a la página ---
+        page.add( DPI, primer_nombre, segundo_nombre, otro_nombre, primer_apellido, segundo_apellido, otro_apellido, usuario,
+                 contraseña, boton_guardar)
 
 
 # Ejecutar la app
-ft.app(target=IngresoEmpleado.ingreso_empleado)
+if __name__ == "__main__":
+    app = IngresoEmpleado()
+    ft.app(target=app.ingreso_empleado)
